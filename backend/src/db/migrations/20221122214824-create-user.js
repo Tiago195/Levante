156823a08b1ce5a17f243a767c7e2e7dc6dfa520
Tiago195 <tiagocf475@gmail.com>
2022-11-22 19:21:24 -0300

    feat: :sparkles: table user

---
+++ backend/src/db/migrations/20221122214824-create-user.js
@@ -0,0 +1,40 @@
+module.exports = {
+  /**
+   * 
+   * @param {import('sequelize').QueryInterface} queryInterface 
+   * @param {import('sequelize')} Sequelize 
+   */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      permission: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
---
+++ backend/src/db/models/user.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @param {import('sequelize').Sequelize} Sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+module.exports = (Sequelize, DataTypes) => {
+  const User = Sequelize.define('User', {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    name: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password: DataTypes.STRING,
+    permission: DataTypes.BOOLEAN,
+    createAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  })
+
+  return User;
+}
