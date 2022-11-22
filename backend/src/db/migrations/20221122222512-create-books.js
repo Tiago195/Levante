371fb0e586b7c0176a6ff8cf10f447fb77678775
Tiago195 <tiagocf475@gmail.com>
2022-11-22 19:32:41 -0300

    feat: :sparkles: table book

---
+++ backend/src/db/migrations/20221122222512-create-books.js
@@ -0,0 +1,43 @@
+module.exports = {
+  /**
+ * 
+ * @param {import('sequelize').QueryInterface} queryInterface 
+ * @param {import('sequelize')} Sequelize 
+ */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Books', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      author: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      status: {
+        type: Sequelize.BOOLEAN,
+        defaultValue: 0
+      },
+      content: {
+        type: Sequelize.STRING
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Books');
+  }
+};
---
+++ backend/src/db/models/books.js
@@ -0,0 +1,22 @@
+/**
+ * 
+ * @param {import('sequelize').Sequelize} Sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+module.exports = (Sequelize, DataTypes) => {
+  const Book = Sequelize.define('Book', {
+    id: {
+      autoIncrement: true,
+      primaryKey: true,
+      type: DataTypes.INTEGER
+    },
+    title: DataTypes.STRING,
+    author: DataTypes.STRING,
+    status: DataTypes.BOOLEAN,
+    content: DataTypes.STRING,
+    createAt: DataTypes.DATE,
+    updatedAt: DataTypes.DATE,
+  })
+
+  return Book;
+}
