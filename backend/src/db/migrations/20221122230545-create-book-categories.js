472733cb7fd320080769220a8e1fc6e0f267ec97
Tiago195 <tiagocf475@gmail.com>
2022-11-22 20:10:59 -0300

    feat: :sparkles: table bookCategories

---
+++ backend/src/db/migrations/20221122230545-create-book-categories.js
@@ -0,0 +1,29 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('BookCategories', {
+      bookId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        references: {
+          model: 'Books',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      },
+      categoryId: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        references: {
+          model: 'Categories',
+          key: 'id'
+        },
+        onDelete: 'CASCADE',
+        onUpdate: 'CASCADE',
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('BookCategories');
+  }
+};
---
+++ backend/src/db/models/bookcategories.js
@@ -0,0 +1,30 @@
+/**
+ * 
+ * @param {import('sequelize').Sequelize} Sequelize 
+ * @param {import('sequelize').DataTypes} DataTypes 
+ */
+module.exports = (Sequelize, DataTypes) => {
+  const BookCategory = Sequelize.define('BookCategory', {
+    bookId: DataTypes.INTEGER,
+    categoryId: DataTypes.INTEGER,
+  }, {
+    timestamps: false
+  })
+
+  BookCategory.associate = ({ Book, Category }) => {
+    Book.belongsToMany(Category, {
+      as: "categories",
+      through: BookCategory,
+      foreignKey: 'bookId',
+      otherKey: 'categoryId'
+    })
+    Category.belongsToMany(Book, {
+      as: "books",
+      through: BookCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'bookId'
+    })
+  }
+
+  return BookCategory;
+}
