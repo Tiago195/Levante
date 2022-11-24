2eebb0b3e2788804fcddf5e1ae878f04dca74d29
Tiago195 <tiagocf475@gmail.com>
2022-11-23 22:29:31 -0300

    refactor: :recycle: add column qtdBooks on Categories

--- backend/src/services/category.service.js
+++ backend/src/services/category.service.js
@@ -1,8 +1,22 @@
-const { Category } = require("../db/models");
+const { Sequelize } = require("sequelize");
+const { Category, Book } = require("../db/models");
 
 module.exports = {
   getAll: async () => {
-    const categories = await Category.findAll();
+    const categories = await Category.findAll({
+      attributes: {
+        include: [[Sequelize.fn("COUNT", Sequelize.col("books.id")), "qtdBooks"]]
+      },
+      include: {
+        as: "books",
+        model: Book,
+        attributes: [],
+        through: { attributes: [] }
+      },
+      group: ["Category.id"],
+    });
+
+    console.log(await categories[1].countBooks());
 
     return categories;
   }
