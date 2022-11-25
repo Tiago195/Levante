a39d72c1280762670b2cc8a2743f44544e56d8d1
Tiago195 <tiagocf475@gmail.com>
2022-11-25 00:32:33 -0300

    feat: :sparkles: validation admin

--- frontend/src/pages/Profile.tsx
+++ frontend/src/pages/Profile.tsx
@@ -1,36 +1,46 @@
 import { Tab, TabList, TabPanel, TabPanels, Tabs } from "@chakra-ui/react";
-import React from "react";
+import React, { useContext } from "react";
 import { Cadastrar } from "../components/Cadastrar";
 import { Header } from "../components/Header";
 import { History } from "../components/History";
 import { Pendentes } from "../components/Pendentes";
 import { Reservar } from "../components/Reservar";
+import Context from "../context";
 
 export const Profile = () => {
+  const { user } = useContext(Context);
+
   return (
     <>
       <Header/>
-
       <Tabs>
         <TabList>
-          <Tab>Cadastrar</Tab>
-          <Tab>Reservar</Tab>
-          <Tab>Pendentes</Tab>
+          {user?.isAdmin && (
+            <>
+              <Tab>Cadastrar</Tab>
+              <Tab>Reservar</Tab>
+              <Tab>Pendentes</Tab>
+            </>
+          )}
           <Tab>Historico de Reservas</Tab>
         </TabList>
 
         <TabPanels>
-          <TabPanel>
-            <Cadastrar />
-          </TabPanel>
+          {user?.isAdmin && (
+            <>
+              <TabPanel>
+                <Cadastrar />
+              </TabPanel>
 
-          <TabPanel>
-            <Reservar />
-          </TabPanel>
+              <TabPanel>
+                <Reservar />
+              </TabPanel>
 
-          <TabPanel>
-            <Pendentes />
-          </TabPanel>
+              <TabPanel>
+                <Pendentes />
+              </TabPanel>
+            </>
+          )}
 
           <TabPanel>
             <History />
