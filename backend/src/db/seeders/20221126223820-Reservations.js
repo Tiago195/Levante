5829d018f201c9376de2206eaf38270fdeb104dd
Tiago195 <tiagocf475@gmail.com>
2022-11-27 12:47:37 -0300

    fix: :seedling: reservation

--- backend/src/db/seeders/20221126223820-Reservations.js
+++ backend/src/db/seeders/20221126223820-Reservations.js
@@ -18,14 +18,14 @@ module.exports = {
         bookId: 1,
         returnPreview: new Date("2022-11-15"),
         status: "Reading",
-        createdAt: new Date("2022-10-06 22:52:18")
+        createdAt: new Date("2022-10-06 13:52:18")
       },
       {
         userId: 2,
         bookId: 6,
         returnPreview: new Date("2022-11-30"),
         status: "Pending",
-        createdAt: new Date("2022-11-26 22:52:18")
+        createdAt: new Date("2022-11-26 15:52:18")
       },
       {
         userId: 2,
@@ -33,7 +33,7 @@ module.exports = {
         returnPreview: new Date("2022-12-31"),
         returnDate: new Date("2022-10-26 22:55:49"),
         status: "Finished",
-        createdAt: new Date("2022-08-16 22:52:18")
+        createdAt: new Date("2022-08-16 13:52:18")
       },
       {
         userId: 3,
@@ -48,7 +48,7 @@ module.exports = {
         returnPreview: new Date("2022-09-17"),
         returnDate: new Date("2022-11-26 23:05:43"),
         status: "Finished",
-        createdAt: new Date("2022-08-16 22:52:18")
+        createdAt: new Date("2022-08-16 13:52:18")
       },
       {
         userId: 3,
@@ -56,7 +56,7 @@ module.exports = {
         returnPreview: new Date("2022-06-01"),
         returnDate: new Date("2022-08-26 23:05:43"),
         status: "Finished",
-        createdAt: new Date("2022-05-16 22:52:18")
+        createdAt: new Date("2022-05-16 13:52:18")
       },
     ]);
   },
