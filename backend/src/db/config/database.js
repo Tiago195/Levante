7be0692fbfc4c82853d6c19f73032484cd9653a8
Tiago195 <tiagocf475@gmail.com>
2022-11-26 21:19:46 -0300

    chore: :construction:

--- backend/src/db/config/database.js
+++ backend/src/db/config/database.js
@@ -3,10 +3,10 @@ require("dotenv").config();
 module.exports = {
   "development": {
     "username": "root",
-    "password": process.env.PASSWORD ?? null,
-    "database": process.env.DATABASE ?? "database_development",
-    "host": process.env.HOST ?? "127.0.0.1",
-    "port": process.env.DB_PORT ?? "3306",
+    "password": process.env.PASSWORD || "password",
+    "database": process.env.DATABASE || "database_development",
+    "host": process.env.HOST || "127.0.0.1",
+    "port": process.env.DB_PORT || "3306",
     "dialect": "mysql"
   }
 };
