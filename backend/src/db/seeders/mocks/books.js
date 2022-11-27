b2ab11c152e8146948539c3c498e3fa1267cfe0b
Tiago195 <tiagocf475@gmail.com>
2022-11-26 22:49:04 -0300

    fix: :bug: seed book

--- backend/src/db/seeders/mocks/books.js
+++ backend/src/db/seeders/mocks/books.js
@@ -19,16 +19,19 @@ const infanto = [
     "title": "O Pequeno Príncipe",
     "author": "Antoine de Saint-exupery",
     "resume": "“Todas as pessoas grandes foram um dia crianças – mas poucas se lembram disso.”",
+    "status": false
   },
   {
     "cover": "https://d1pkzhm5uq4mnt.cloudfront.net/imagens/capas/_c2a1cab66bc09e2bee42344c8d9351253ae44a29.jpg",
     "title": "Dom Quixote",
-    "author": "Miguel de Cervantes"
+    "author": "Miguel de Cervantes",
+    "status": false
   },
   {
     "cover": "https://d1pkzhm5uq4mnt.cloudfront.net/imagens/capas/11b151f877723363abfc3b5440716b6ce5be8dc9.jpg",
     "title": "Pollyanna",
     "author": "Eleanor H. Porter",
+    "status": false
   },
 ];
 
@@ -42,12 +45,14 @@ const jogos = [
   {
     "cover": "https://d1pkzhm5uq4mnt.cloudfront.net/imagens/capas/a41b38f29ac86f6f245cb5b00bc4e7c59c48767e.jpg",
     "title": "Jogo de Paciência",
-    "author": "M. Lover"
+    "author": "M. Lover",
+    "status": false
   },
   {
     "cover": "https://d1pkzhm5uq4mnt.cloudfront.net/imagens/capas/7eebc1b7e3fc431c674496fbd2ab84bddad15847.jpg",
     "title": "O Livro Verde do Pôquer - Texas Holdem",
     "author": "Phil Gordon",
+    "status": false
   },
 ];
 
@@ -55,17 +60,20 @@ const poesia = [
   {
     "cover": "https://d1pkzhm5uq4mnt.cloudfront.net/imagens/capas/dede3aafb55187986a98828672e827f93b47be65.jpg",
     "title": "Espumas Flutuantes",
-    "author": "Castro Alves"
+    "author": "Castro Alves",
+    "status": false
   },
   {
     "cover": "https://d1pkzhm5uq4mnt.cloudfront.net/imagens/capas/a6e385d18b7e8b0ea91ea1e4faa809808cb0326c.jpg",
     "title": "Os Lusíadas",
-    "author": "Luís de Camões"
+    "author": "Luís de Camões",
+    "status": false
   },
   {
     "cover": "https://d1pkzhm5uq4mnt.cloudfront.net/imagens/capas/5976b7bcc3cd4381e9fa7ebbc329f6a6c9d91175.jpg",
     "title": "Poemas de Álvaro de Campos",
     "author": "Fernando Pessoa",
+    "status": false,
     "resume": "“Toda a gente sabe hoje, depois de o saber, que há geometrias chamadas não-euclidianas, isto é, que partem de postulados diferentes dos de Euclides, e chegam a conclusões diferentes. Estas geometrias têm cada uma um desenvolvimento lógico: são sistemas interpretativos independentes, independentemente aplicáveis à realidade. Foi fecundo em matemática e além da matemática (Einstein bastante lhe deve) este processo de multiplicar as geometrias «verdadeiras», e fazer, por assim dizer, abstrações de vários tipos na mesma realidade objetiva.”"
   },
 ];
@@ -74,17 +82,20 @@ const ficcao = [
   {
     "cover": "https://d1pkzhm5uq4mnt.cloudfront.net/imagens/capas/f6b23e343f15fd9dcdfba8a09143e86b6cd02a7f.jpg",
     "title": "Praticamente Inofensiva",
-    "author": "Douglas Adams"
+    "author": "Douglas Adams",
+    "status": false
   },
   {
     "cover": "https://d1pkzhm5uq4mnt.cloudfront.net/imagens/capas/b3df2e18389feed0c7d96ef7f61f99836bc0710b.jpg",
     "title": "Fundação",
-    "author": "Isaac Asimov"
+    "author": "Isaac Asimov",
+    "status": false
   },
   {
     "cover": "https://d1pkzhm5uq4mnt.cloudfront.net/imagens/capas/_05922f0eb6f694e04aa4b3cdd385d0cdc53c0f0b.jpg",
     "title": "A Vida, o Universo e Tudo Mais",
-    "author": "Douglas Adams"
+    "author": "Douglas Adams",
+    "status": false
   },
 ];
 
